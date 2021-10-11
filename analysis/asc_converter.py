a710b0416d1d59d17ff6934e1f426b0439a81e25
Alex <hoogerbrugge.alex@gmail.com>
2021-10-11 10:23:51 +0200

    asc_converter fixes

--- analysis/asc_converter.py
+++ analysis/asc_converter.py
@@ -11,6 +11,52 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-----------------------------------------------
+
+Use SR Research's EDFConverter or EDF2ASC, with the following settings (v = on, x = off):
+v UTF Encoding
+- Samples/Events -> Output events only
+- Binocular recording -> Output must be monocular (setting output as binocular for a monocular recording also works)
+
+x Output resolution data
+v Output velocity data
+v Output float time
+x Output input values
+
+- Eye position type -> Gaze
+x Load EB log messages
+
+v Block start event output
+v Block message event output
+x Block eye event output
+v Block flags output
+x Block target data
+v Block viewer commands
+
+v Use tabs only as delimiters
+
+If settings match, these should be the columns that originate in the .asc file, dependent on whether it's a SACC or FIX
+(note that the columns have no names, so a quick count of the number of columns should help)
+columns_sacc = ['event_type',
+                'eye',
+                'start_time',
+                'end_time',
+                'duration',
+                'start_x',
+                'start_y',
+                'end_x',
+                'end_y',
+                'amplitude',
+                'peak_vel']
+columns_fix = ['event_type',
+               'eye',
+               'start_time',
+               'end_time',
+               'duration',
+               'avg_x',
+               'avg_y',
+               'avg_pupilsize']
 """
 
 from pathlib import Path
@@ -20,11 +66,10 @@ import numpy as np
 import pandas as pd
 from helperfunctions import euclidean_distance
 
-# TODO: description of EDFConverter settings etc.
 
 def convert_fix(row: List[str]) -> pd.DataFrame:
-    # Extract al necessary information and return a dataframe row with columns=columns
-    row_dict = {'type' : 'fixation',
+    # Extract al necessary information and return a dataframe row
+    row_dict = {'type': 'fixation',
                 'start': float(row[2]),
                 'end': float(row[3]),
                 'dur': float(row[4]),
@@ -44,14 +89,14 @@ def convert_fix(row: List[str]) -> pd.DataFrame:
 
 
 def convert_sacc(row: List[str]) -> pd.DataFrame:
-    # Extract al necessary information and return a dataframe row with columns=columns
+    # Extract al necessary information and return a dataframe row
 
     # Avg velocity = distance (px) / time (ms)
     dur, gstx, gsty, genx, geny = float(row[4]), float(row[5]), float(row[6]), float(row[7]), float(row[8])
     dist = euclidean_distance((gstx, gsty), (genx, geny))
     avel = dist / dur
 
-    row_dict = {'type' : 'saccade',
+    row_dict = {'type': 'saccade',
                 'start': float(row[2]),
                 'end': float(row[3]),
                 'dur': dur,
@@ -61,7 +106,7 @@ def convert_sacc(row: List[str]) -> pd.DataFrame:
                 'geny': geny,
                 'gavx': np.nan,
                 'gavy': np.nan,
-                'avel': avel,  # Don't know
+                'avel': avel,  # Can't know
                 'pvel': float(row[10]),
                 'pups': np.nan}
 
@@ -70,76 +115,59 @@ def convert_sacc(row: List[str]) -> pd.DataFrame:
     return row_df
 
 
-# These are the columns that originate in the .asc file, dependent on whether it's a SACC or FIX
-# columns_sacc = ['event_type',
-#                 'eye',
-#                 'start_time',
-#                 'end_time',
-#                 'duration',
-#                 'start_x',
-#                 'start_y',
-#                 'end_x',
-#                 'end_y',
-#                 'amplitude',
-#                 'peak_vel'
-#                 ]
-# columns_fix = ['event_type',
-#                'eye',
-#                'start_time',
-#                'end_time',
-#                'duration',
-#                'avg_x',
-#                'avg_y',
-#                'avg_pupilsize'
-#                ]
+if __name__ == '__main__':
+    # Set path to results folder
+    path = Path(__file__).parent.parent
+    path = path / 'results'
 
-# Set path to results folder
-path = Path(__file__).parent.parent
-path = path / 'results'
+    # Search all .asc files
+    files = list(path.rglob('*.asc'))
+    print(f'Extracting data from {len(files)} files and converting from .asc to .csv')
 
-# Search all .asc files
-files = list(path.glob('*.asc'))
-print(f'Extracting data from {len(files)} files and converting from .asc to .csv')
+    # Loop through files
+    for file in files:
+        asc = open(file, 'r')
+        f = asc.read()
 
-for file in files:
-    asc = open(file, 'r')
-    f = asc.read()
+        # f is now one long string, so split into list of separate rows
+        rows = f.split('\n')
 
-    # f is now one long string, so split into list of separate rows
-    rows = f.split('\n')
+        # Find in which rows a measurement was started. Namely, we only need the last 'block' of measurement
+        start_rows = []
+        for i, row in enumerate(rows):
+            if row.startswith('EVENTS\tGAZE'):
+                start_rows.append(i)
 
-    # Find in which rows a measurement was started. Namely, we only need the last 'block' of measurement
-    start_rows = []
-    for i, row in enumerate(rows):
-        if row.startswith('EVENTS\tGAZE'):
-            start_rows.append(i)
+        # Take the last index of starting rows
+        starting_row = start_rows[-1] + 1
 
-    # Take the last index of starting rows
-    starting_row = start_rows[-1] + 1
+        # Remove everything before the new starting point
+        rows = rows[starting_row:-1]
 
-    # Remove everything before the new starting point
-    rows = rows[starting_row:-1]
+        # Filter for fixations and saccades
+        new_rows = []
+        for row in rows:
+            if row.startswith('EFIX') or row.startswith('ESACC'):
+                split_row = row.split('\t')
+                new_rows.append(split_row)
 
-    # Filter for fixations and saccades
-    new_rows = []
-    for row in rows:
-        if row.startswith('EFIX') or row.startswith('ESACC'):
-            split_row = row.split('\t')
-            new_rows.append(split_row)
+        # Create an empty dataframe and fill in per row, based on whether it's a FIX or SACC
+        df = pd.DataFrame(columns=['type', 'start', 'end', 'dur', 'gstx', 'gsty', 'genx', 'geny', 'gavx', 'gavy',
+                                   'avel', 'pvel', 'pups'])
+        for row in new_rows:
+            if row[0] == 'EFIX':
+                converted_row = convert_fix(row)
+            elif row[0] == 'ESACC':
+                converted_row = convert_sacc(row)
+            else:
+                pass  # else-statement for readability, to show that we're doing only fixes and saccades
 
-    df = pd.DataFrame(columns=['type', 'start', 'end', 'dur', 'gstx', 'gsty', 'genx', 'geny', 'gavx', 'gavy', 'avel', 'pvel', 'pups'])
-    for row in new_rows:
-        if row[0] == 'EFIX':
-            converted_row = convert_fix(row)
-        elif row[0] == 'ESACC':
-            converted_row = convert_sacc(row)
-        else:
-            raise ValueError("I'm just doing this for clarity")
+            df = df.append(converted_row, ignore_index=True)
 
-        df = df.append(converted_row, ignore_index=True)
+        # Write to csv
+        new_path = str(file).replace('.asc', '-events.csv')
+        df.to_csv(Path(new_path))
 
-    # Write to csv
-    new_path = str(file).replace('.asc', '.csv')
-    df.to_csv(Path(new_path))
+    print(f'Converted {len(files)} files!')
 
-print(f'Converted {len(files)} files!')
+
